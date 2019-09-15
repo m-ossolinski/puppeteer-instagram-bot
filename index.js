53fa49c5d99b207c500cc1f14df135a11e30b8ef
unknown <mateusz.ossolinski@gmail.com>
2019-09-15 20:43:08 +0200

    add passwords to separate file

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const ig = require('./instagram');
+const logins = require('./passwords');
 
 (async () => {
 
   await ig.initialize();
 
-  await ig.login('qweqwe', 'qweqw');
+  await ig.login(logins.logins.login, logins.logins.password);
+
+  await ig.likeTagsProcess(['javascript', 'nodejs', 'tech', 'frontend', 'backend', 'frontenddeveloper', 'programista', 'testing', 'helloworld']);
+
   debugger;
 
 })();
--- instagram.js
+++ instagram.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 
 const BASE_URL = 'https://instagram.com';
+const TAG_URL = (tag) => `https://www.instagram.com/explore/tags/${tag}/`;
 
 const instagram = {
   browser: null,
@@ -26,7 +27,7 @@ const instagram = {
      // click login btn
     await loginButton[0].click();
 
-    await instagram.page.waitForNavigation({ waitUntil: 'networkidle2' });
+    // await instagram.page.waitForNavigation({ waitUntil: 'networkidle2' });
 
     await instagram.page.waitFor(1000);
 
@@ -34,7 +35,55 @@ const instagram = {
     await instagram.page.type('input[name="username"]', username, { delay: 50 });
     await instagram.page.type('input[name="password"]', password, { delay: 50 });
 
-    debugger;
+
+    // login btn
+
+    loginButton = await instagram.page.$x('//div[contains(text(), "Zaloguj się")]');
+    await loginButton[0].click();
+
+    await instagram.page.waitFor(10000);
+    await instagram.page.waitFor('a > span[aria-label="Profil"')
+  },
+
+  likeTagsProcess: async (tags = []) => {
+
+    for(let tag of tags) {
+      // go to tag page
+
+      await instagram.page.goto(TAG_URL(tag), { waitUntil: 'networkidle2' });
+      await instagram.page.waitFor(1000);
+
+      let posts = await instagram.page.$$('article > div:nth-child(3) img[decoding="auto"]');
+
+      for(let i = 0; i < 3; i++) {
+
+        let post = posts[i];
+
+        // click on post
+
+        await post.click();
+        await instagram.page.waitFor('span[id="react-root"][aria-hidden="true"]');
+        await instagram.page.waitFor(1000);
+
+        let isLikeable = await instagram.page.$('span[aria-label="Lubię to!"]');
+
+        if(isLikeable) {
+          await instagram.page.click('span[aria-label="Lubię to!"]');
+        }
+
+        await instagram.page.waitFor(3000);
+
+        // close post
+        let closeModalButton = await instagram.page.$x('//button[contains(text(), "Zamknij")]');
+        await closeModalButton[0].click();
+
+
+        await instagram.page.waitFor(1000);
+      }
+
+      await instagram.page.waitFor(10000);
+
+    }
 
   }
 };
